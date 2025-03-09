5c87a51cc36ab3b849c6b6f72b401c531fa07bd5
UnintelligableGoofball <unfixedsoup@proton.me>
2025-03-09 18:14:07 +0000

    Maybe Working Starlight

--- src/c/starlightwatch.c
+++ src/c/starlightwatch.c
@@ -13,44 +13,10 @@ static Window *s_main_window;
 static TextLayer *s_time_layer;
 static GFont s_time_font;
 
-static BitmapLayer *s_bitmap_layer, *s_hands_layer;
-static GBitmap *s_background_bitmap, *s_hands_bitmap;
+static BitmapLayer *s_bitmap_layer;
+static GBitmap *s_background_bitmap;
 
 static ClaySettings s_settings;
-
-static void prv_unobstructed_will_change(GRect final_unobstructed_window, void *context) {
-  GRect full_bounds = layer_get_bounds(window_get_root_layer(s_main_window));
-  
-  if (!grect_equal(&full_bounds, &final_unobstructed_window)) {
-    // Screen is about to become obstructed
-
-    //bitmap has height of 7, needs to be at bottom
-    s_hands_layer = bitmap_layer_create(
-      GRect(0, final_unobstructed_window.size.h - 7, final_unobstructed_window.size.w, 7));
-
-    s_hands_bitmap = gbitmap_create_with_resource(RESOURCE_ID_HANDS_OVERLAY);
-    bitmap_layer_set_bitmap(s_hands_layer, s_hands_bitmap);
-    bitmap_layer_set_background_color(s_hands_layer, GColorClear);
-    bitmap_layer_set_compositing_mode(s_hands_layer, GCompOpSet);
-
-    layer_add_child(window_get_root_layer(s_main_window), bitmap_layer_get_layer(s_hands_layer));
-  }
-
-}
-
-static void prv_unobstructed_did_change(void *context) {
-  GRect full_bounds = layer_get_bounds(window_get_root_layer(s_main_window));
-  GRect bounds = layer_get_unobstructed_bounds(window_get_root_layer(s_main_window));
-
-  if (grect_equal(&full_bounds, &bounds)) {
-    //quickview gone
-
-    layer_remove_from_parent(bitmap_layer_get_layer(s_hands_layer));
-    bitmap_layer_destroy(s_hands_layer);
-    gbitmap_destroy(s_hands_bitmap);
-  } 
-}
-
 
 static void update_time(){
   time_t temp = time(NULL);
@@ -132,10 +98,6 @@ static void main_window_load(Window *window) {
 
 }
 
-static void prv_update_animations() {
-
-}
-
 static void main_window_unload(Window *window) {
 
 }
@@ -189,13 +151,6 @@ static void init() {
     .load = main_window_load,
     .unload = main_window_unload
   });
-
-  UnobstructedAreaHandlers unobstructed_handler = (UnobstructedAreaHandlers) {
-    .will_change = prv_unobstructed_will_change,
-    .did_change = prv_unobstructed_did_change
-  };
-
-  unobstructed_area_service_subscribe(unobstructed_handler, NULL);
 
   window_stack_push(s_main_window, true);
 
